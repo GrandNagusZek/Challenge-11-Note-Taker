6eeeba8ce0c837352168ced69c06e7de9b13db1c
GrandNagusZek <iliyas.mallard@yahoo.com>
2024-03-24 21:36:37 -0400

    saved

--- unmodularized_server.js
+++ unmodularized_server.js
@@ -2,6 +2,8 @@
 const express=require("express")
 // need path to find the file path
 const path=require("path")
+const fs=require("fs")
+const {v4: uuidv4} = require("uuid")
 // need port to identify the server, Heroku will assign a port number, this is what the 'process.env.PORT' is for...the || statement is for when/if we run locally the port will be 3001
 const PORT = process.env.PORT || 3001 
 
@@ -19,6 +21,18 @@ app.use(express.urlencoded({extended:true}))
 //we need 2 types of routes html and api
 
 //api routes
+
+
+app.get("/api/notes", (req, res)=>{
+
+})
+
+
+
+
+
+
+app.post("/api/notes")
 
 
 
