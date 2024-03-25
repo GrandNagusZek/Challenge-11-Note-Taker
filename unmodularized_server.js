a6f323d294b9892f8dc95a3978d38ce26c4be135
GrandNagusZek <iliyas.mallard@yahoo.com>
2024-03-25 11:37:41 -0400

    added post route to unmodularized_server.js

--- unmodularized_server.js
+++ unmodularized_server.js
@@ -2,8 +2,7 @@
 const express=require("express")
 // need path to find the file path
 const path=require("path")
-const fs=require("fs")
-const {v4: uuidv4} = require("uuid")
+
 // need port to identify the server, Heroku will assign a port number, this is what the 'process.env.PORT' is for...the || statement is for when/if we run locally the port will be 3001
 const PORT = process.env.PORT || 3001 
 
@@ -16,6 +15,9 @@ const app=express()
 app.use(express.json())
 //extended:true means we want the exact data from teh original after parsing
 app.use(express.urlencoded({extended:true}))
+app.use(express.static("public"))
+const fs =require("fs")
+
 
 
 //we need 2 types of routes html and api
@@ -24,32 +26,9 @@ app.use(express.urlencoded({extended:true}))
 
 
 app.get("/api/notes", (req, res)=>{
-
-})
-
-
-
-
-
-
-app.post("/api/notes")
-
-
-
-
-//html routes
-// app.get("*") is like a default path
-app.get("*", (req, res)=>{
-    //path.join it looks for that file path and once it finds it, it joins the file and the folder together. Here '_dirname' is the folder name
-    res.sendFile(path.join(__dirname, './public/index/html'))
-})
-
-
-app.use("/notes" , ()=>{
-    //res is the response, 'sendFile' is the response that you want to send back to the browser
-    res.sendFile(err=>{
-        //path.join is a way to combine 2 paths together
-        path.join(__dirname , "./notes.html" ) 
+    fs.readFile("./db/db.json", "utf8", (err, data)=> {
+        const newData = JSON.parse(data)
+        res.json(newData)
     })
 })
 
@@ -58,8 +37,51 @@ app.use("/notes" , ()=>{
 
 
 
+app.post("/api/notes", (req, res)=> {
+    const { title, text} = req.body
+    
+    if (title && text) {
+        const newNote = {title, text, id: uuid()}
+
+        fs.readFile('./db/db.json', 'utf8', (err, data) =>{
+            if (err) {
+                console.error(err)
+                return res.status(500).json({error:"Error reading notes data"})
+            }
+            const notes = JSON.parse(data)
+            notes.push(newNote)
+            db=newNote
+            fs.writeFile('./db/db.json', JSON.stringify(notes, null, 4), (err) => {
+                if(err) {
+                    console.err(err);
+                    return res.status(500).json({error:'Error writing new note'});
+                }
+                res.json(db);
+                console.info(`${req.method}request to add a new note was successful`)
+            });
+        });
+    }else {
+        res.status(400).json({error: "Please provide both a title and text for the note"})
+    }
+})
+
+
+app.get("/notes", (req, res)=>{
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+})
+
+//html routes
+// app.get("*") is like a default path
+app.get("*", (req, res)=>{
+    //path.join it looks for that file path and once it finds it, it joins the file and the folder together. Here '_dirname' is the folder name
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+
+
+
 app.listen(PORT, () => {
-    console.log("App is listening at PORT: http://localhost:3001/" + PORT)
+    console.log("App is listening at PORT: http://localhost:" + PORT)
 })
 
 
